529dd2f7fa80fcfbea381334a06c019e786a45d5
lbeck37 <lbeck37@users.noreply.github.com>
2021-05-27 10:54:53 -0600

    Cleaned out code that was for PowerShift Coach

--- Arduino/Sketches/BeckE32_RoverDisplayTest/BeckE32_RoverDisplayTest.ino
+++ Arduino/Sketches/BeckE32_RoverDisplayTest/BeckE32_RoverDisplayTest.ino
@@ -1,5 +1,5 @@
 const char szSketchName[]  = "BeckE32_RoverDisplayTest.ino";
-const char szFileDate[]    = "5/27/21c";
+const char szFileDate[]    = "5/27/21e";
 // 5/26/21, Copied from BeckE32_RoverGraphics.ino to isolate white screen problem
 #include <BeckI2cClass.h>
 #include <BeckGasSensorClass.h>
@@ -16,16 +16,6 @@ const char szFileDate[]    = "5/27/21c";
 #include <Streaming.h>
 
 #define min(X, Y)     (((X) < (Y)) ? (X) : (Y))
-#define PAUSE_DELAY   delay(2000)
-#define RADIX_10      10
-#define DO_BUTTONS    true
-#define USE_DOGS102   false
-
-static const int       sNormalMode           = 0;
-static const int       sCalibMode            = 1;
-static const int       sUp                   = 0;
-static const int       sDown                 = 1;
-static const boolean   bButtonPullUp         = true;
 
 //Digital Pins
 static const int       sI2C_SDA              = 26;
@@ -33,48 +23,10 @@ static const int       sI2C_SCL              = 27;
 
 static const uint16_t  usBackgroundColor    = WROVER_BLACK;
 static const UINT16    usBoostTop           = 90;
-static const UINT16    usAccelMotorTop      = 135;
-static const UINT16    usMotorLeft          = 130;
-
-static int sCurrentGear                   = 2;
-
-static int sCurrentMode                   = sNormalMode;
-static int sServoPosLast                  = 0;
 
 WROVER_KIT_LCD    RoverLCD;
-
-//Number of not handled presses, up to sMaxButtonPresses
-static int              sButtonCount[]           = { 0, 0, 0};
-/*
-static boolean          abButtonBeingHeld[]      = { false, false, false};
-static unsigned long    ulNextModeTime           = 0;  //msec when a mode switch can take place
-static unsigned long    ulModeReadyTime          = 0;  //msec when button presses can be handled
-*/
-static boolean          bButtonsChanged          = true;
-static boolean          bGearChanged             = true;
-static boolean          bServoChanged            = true;
-static boolean          bModeChanged             = true;
-static boolean          bGyroChanged             = true;
-//static boolean          bCO2Changed              = true;
-//static boolean          bVOCChanged              = true;
-static boolean          bMotorChanged            = true;
-static boolean          bMPHChanged              = true;
-
-//static int         sLineCount= 0;     //Used in outputs to Serial Monitor for clarity.
-static char       szTempBuffer[100];   //DOGS102 line is 17 chars with 6x8 normal font.
 static char       sz100CharString[101];
 
-const double      dGConvert         = 16383.0;  // 12/06/17 I'm seeing values up to -16,580 with a vertical orientation, 0x3FFF = 16,383
-const double      dRadsToDeg        = 180.0/PI;
-double            adGvalueXYZ[3];
-double            dRollDeg;
-double            dPitchDeg;
-double            dPitchPercent   = 0.0;
-double            dBoostWatts     = 0.0;
-double            dMPH            = 0.0;
-double            dMotorVolts     = 0.0;
-double            dMotorAmps      = 0.0;
-double            dMotorWatts     = 0.0;
 
 void(* ResetESP32)(void)= 0;        //Hopefully system crashes and reset when this is called.
 
@@ -86,15 +38,16 @@ void setup()   {
   I2C_Object.Setup();
   GasSensor.Setup();
   DisplayBegin();
-  bButtonsChanged= true;  //Make the display show up during debugging.
   return;
 }  //setup
+
 
 void loop() {
   GasSensor.Handle();
   DisplayUpdate();
   return;
 }  //loop()
+
 
 void DisplayBegin() {
   Serial << "DisplayBegin(): Call RoverLCD.begin()" << endl;
@@ -103,6 +56,7 @@ void DisplayBegin() {
   DisplayClear();
   return;
 }  //DisplayBegin
+
 
 void ShowStartScreen(void) {
   Serial << "ShowStartScreen(): Call DisplayBegin()" << endl;
@@ -114,10 +68,12 @@ void ShowStartScreen(void) {
   return;
 }  //ShowStartScreen
 
+
 void ShowSplash(void) {
    DisplayClear();
    return;
 }  //ShowSplash
+
 
 void DisplayUpdate(void) {
   DisplayCO2();
@@ -126,15 +82,18 @@ void DisplayUpdate(void) {
   return;
 }  //DisplayUpdate
 
+
 void DisplayClear() {
   FillScreen(usBackgroundColor);
   return;
 }  //DisplayClear
 
+
 void FillScreen(UINT16 usColor) {
   RoverLCD.fillScreen(usColor);
   return;
 }  //FillScreen
+
 
 void DisplayText(UINT16 usCursorX, UINT16 usCursorY, char *pcText,
                  const GFXfont *pFont, UINT8 ucSize, UINT16 usColor) {
@@ -148,10 +107,12 @@ void DisplayText(UINT16 usCursorX, UINT16 usCursorY, char *pcText,
   return;
 }  //DisplayText
 
+
 void ClearTextBackground(INT16 sUpperLeftX, INT16 sUpperLeftY, UINT16 usWidth, UINT16 usHeight){
   RoverLCD.fillRect(sUpperLeftX, sUpperLeftY, usWidth, usHeight, usBackgroundColor);
   return;
 } //ClearTextBackground
+
 
 void DisplayLine(const GFXfont stFont, UINT16 usColor, UINT16 usCursorX, UINT16 usCursorY, UINT16 usClearWidth, UINT16 usClearHeight,
                  char szText[], bool bClearText= true, UINT8 ucSize= 1) {
@@ -165,18 +126,18 @@ void DisplayLine(const GFXfont stFont, UINT16 usColor, UINT16 usCursorX, UINT16 
   return;
 } //DisplayLine
 
+
 void DisplayLowerBanner(){
   const GFXfont   *pFont          = &FreeSansOblique18pt7b;
   UINT16          usCursorX       = 7;
   UINT16          usCursorY       = 232;    //Was 72
   UINT8           ucSize          = 1;
   UINT16          usColor         = WROVER_CYAN;
-  bool            bRightJustify   = false;
 
-//DisplayText( usCursorX, usCursorY, "PowerShift Coach", pFont, ucSize, usColor);
   DisplayText( usCursorX, usCursorY, "      Air Quality", pFont, ucSize, usColor);
   return;
 } //DisplayLowerBanner
+
 
 void DisplayCO2() {
   UINT16          usCharWidth     = 25;
@@ -184,31 +145,30 @@ void DisplayCO2() {
   UINT16          usCursorY       = 30;   //GFX fonts Y is bottom
   UINT8           ucSize          = 1;
   UINT16          usColor         = WROVER_WHITE;
-  UINT16          usRightInset    = 2;  //Number of pixels to right of justified text
   INT16           sClearLeftX     = usCursorX;
   INT16           sClearTopY      = 0;
   UINT16          usClearWidth    = 120;
   UINT16          usClearHeight   = 35;
   static UINT16   usLastClearWidth= 0;
 
-  //Serial << "DisplayCO2(): Begin" << endl;
   if(GasSensorData.bCO2Changed()) {
     UINT16 CO2Value= GasSensorData.GetCO2_Value();
-    sprintf(szTempBuffer, "%6d", CO2Value);
+    sprintf(sz100CharString, "%6d", CO2Value);
     //Calculate width to clear based on number of characters + 2, use that unless last width was bigger
-    usClearWidth= (strlen(szTempBuffer) + 2) * usCharWidth;
+    usClearWidth= (strlen(sz100CharString) + 2) * usCharWidth;
     usClearWidth= std::max(usClearWidth, usLastClearWidth);
     usLastClearWidth= usClearWidth;
     ClearTextBackground(sClearLeftX, sClearTopY, usClearWidth, usClearHeight);
-    DisplayLine(FreeMonoBold24pt7b, usColor, usCursorX, usCursorY, usClearWidth, usClearHeight, szTempBuffer, false, ucSize);
+    DisplayLine(FreeMonoBold24pt7b, usColor, usCursorX, usCursorY, usClearWidth, usClearHeight, sz100CharString, false, ucSize);
 
     usCursorX= 50;
     usCursorY += 20;
-    sprintf(szTempBuffer, "CO2 ppm");
-    DisplayLine(FreeSans9pt7b, usColor, usCursorX, usCursorY, usClearWidth, usClearHeight, szTempBuffer, false);
+    sprintf(sz100CharString, "CO2 ppm");
+    DisplayLine(FreeSans9pt7b, usColor, usCursorX, usCursorY, usClearWidth, usClearHeight, sz100CharString, false);
   } //if(GasSensorData.bCO2Changed())
   return;
 }  //DisplayCO2
+
 
 void DisplayVOC() {
   UINT16          usCharWidth     = 25;
@@ -226,157 +186,19 @@ void DisplayVOC() {
     UINT16  VOCValue_ppm      = GasSensorData.GetVOC_Value();
     float   VOC_to_mg_per_m3  = 3.23;
     float   VOC_mgPerM3       = (float)VOCValue_ppm * VOC_to_mg_per_m3;
-    sprintf(szTempBuffer, "%6.1f", VOC_mgPerM3);
+    sprintf(sz100CharString, "%6.1f", VOC_mgPerM3);
     //Calculate width to clear based on number of characters + 2, use that unless last width was bigger
-    usClearWidth= (strlen(szTempBuffer) + 2) * usCharWidth;
+    usClearWidth= (strlen(sz100CharString) + 2) * usCharWidth;
     usClearWidth= std::max(usClearWidth, usLastClearWidth);
     usLastClearWidth= usClearWidth;
     ClearTextBackground(sClearLeftX, sClearTopY, usClearWidth, usClearHeight);
-    DisplayLine(FreeMonoBold24pt7b, usColor, usCursorX, usCursorY, usClearWidth, usClearHeight, szTempBuffer, false, ucSize);
+    DisplayLine(FreeMonoBold24pt7b, usColor, usCursorX, usCursorY, usClearWidth, usClearHeight, sz100CharString, false, ucSize);
 
     usCursorX= 50;
     usCursorY += 20;
-    sprintf(szTempBuffer, "VOC mg/m^3");
-    DisplayLine(FreeSans9pt7b, usColor, usCursorX, usCursorY, usClearWidth, usClearHeight, szTempBuffer, false);
+    sprintf(sz100CharString, "VOC mg/m^3");
+    DisplayLine(FreeSans9pt7b, usColor, usCursorX, usCursorY, usClearWidth, usClearHeight, sz100CharString, false);
   } //if(GasSensorData.bVOCChanged())
   return;
 }  //DisplayVOC
-
-/*
-void DisplayCurrentGear() {
-  UINT16          usCharWidth     = 120;
-  UINT16          usCursorX       = 200;
-  UINT16          usCursorY       = 62;   //GFX fonts Y is bottom
-  UINT8           ucSize          = 2;
-  //UINT16          usRightInset    = 2;  //Number of pixels to right of justified text
-  INT16           sClearLeftX     = usCursorX;
-  INT16           sClearTopY      = 0;
-  UINT16          usClearWidth    = 120;
-  UINT16          usClearHeight   = usCursorY + 10;
-  UINT16          usColor         = WROVER_WHITE;
-
-  //Serial << "DisplayCurrentGear(): Begin" << endl;
-  if(bGearChanged) {
-    if (sCurrentMode == sNormalMode) {
-      sprintf(szTempBuffer, "%2d", sCurrentGear);
-      usClearWidth= strlen(szTempBuffer) * usCharWidth;
-      ClearTextBackground(sClearLeftX, sClearTopY, usClearWidth, usClearHeight);
-      DisplayLine(FreeMonoBold24pt7b, usColor, usCursorX, usCursorY, usClearWidth, usClearHeight, szTempBuffer, false, ucSize);
-    }  //if (sCurrentMode..
-    else {
-      sprintf(szTempBuffer, "%2d", 0);
-      usClearWidth= strlen(szTempBuffer) * usCharWidth;
-      ClearTextBackground(sClearLeftX, sClearTopY, usClearWidth, usClearHeight);
-      DisplayLine(FreeMonoBold24pt7b, usColor, usCursorX, usCursorY, usClearWidth, usClearHeight, szTempBuffer, false, ucSize);
-    }  //if (sCurrentMode..else
-
-    usCursorX= 255;
-    usCursorY += 20;
-    sprintf(szTempBuffer, "Gear");
-    DisplayLine(FreeSans9pt7b, usColor, usCursorX, usCursorY, usClearWidth, usClearHeight, szTempBuffer, false);
-  } //if(bGearChanged)
-  return;
-}  //DisplayCurrentGear
-
-void DisplayServoPos() {
-  const UINT16    usCharWidth   = 17;
-  const UINT16    usLineHeight  = 20;
-  UINT16          usCursorX     = 252;
-  UINT16          usCursorY     = 115;
-  UINT16          usClearWidth  = 70;
-  UINT16          usClearHeight = 22;
-  UINT16          usColor       = WROVER_WHITE;
-
-  //Serial << "DisplayServoPos(): Begin" << endl;
-  if(bServoChanged) {
-    sprintf(szTempBuffer, "%3d", sServoPosLast);
-    usClearWidth= strlen(szTempBuffer) * usCharWidth;
-    DisplayLine(FreeMonoBold12pt7b, usColor, usCursorX + 15, usCursorY, usClearWidth, usClearHeight, szTempBuffer);
-
-    sprintf(szTempBuffer, "Servo");
-    usClearWidth= strlen(szTempBuffer) * usCharWidth;
-    usCursorY += usLineHeight;
-    DisplayLine(FreeSans9pt7b, usColor, usCursorX, usCursorY, usClearWidth, usClearHeight, szTempBuffer, false);
-  } //if(bServoChanged)
-  return;
-}  //DisplayServoPos
-
-void DisplayMPH() {
-  const UINT16    usCharWidth   = 17;
-  const UINT16    usLineHeight  = 20;
-  UINT16          usCursorX     = 252;
-  UINT16          usCursorY     = 175;
-  UINT16          usClearWidth  = 70;
-  UINT16          usClearHeight = 22;
-  UINT16          usColor       = WROVER_YELLOW;
-
-  //Serial << "DisplayMPH(): Begin" << endl;
-  if(bMPHChanged) {
-    sprintf(szTempBuffer, "%4.1f", dMPH);
-    usClearWidth= strlen(szTempBuffer) * usCharWidth;
-    DisplayLine(FreeMonoBold12pt7b, usColor, usCursorX, usCursorY, usClearWidth, usClearHeight, szTempBuffer);
-
-    sprintf(szTempBuffer, "MPH");
-    usClearWidth= strlen(szTempBuffer) * usCharWidth;
-    usCursorY += usLineHeight;
-    DisplayLine(FreeSans9pt7b, usColor, usCursorX, usCursorY, usClearWidth, usClearHeight, szTempBuffer, false);
-  } //if(bMPHChanged)
-  return;
-}  //DisplayMPH
-
-void DisplayMotor() {
-  const UINT16    usCharWidth   = 17;
-  const UINT16    usLineHeight  = 20;
-  UINT16          usCursorX     = usMotorLeft;
-  UINT16          usCursorY     = usAccelMotorTop;
-  UINT16          usClearWidth;
-  UINT16          usClearHeight = 22;
-
-  //Serial << "DisplayMotor(): Begin" << endl;
-  sprintf(szTempBuffer, "Motor");
-  if(bMotorChanged) {
-    usClearWidth= strlen(szTempBuffer) * usCharWidth;
-    DisplayLine(FreeMonoBold12pt7b, WROVER_YELLOW, usCursorX, usCursorY, usClearWidth, usClearHeight, szTempBuffer, false);
-
-    sprintf(szTempBuffer, "V  %4.1f", dMotorVolts);
-    usClearWidth= strlen(szTempBuffer) * usCharWidth;
-    usCursorY += usLineHeight;
-    DisplayLine(FreeMonoBold12pt7b, WROVER_YELLOW, usCursorX, usCursorY, usClearWidth, usClearHeight, szTempBuffer);
-
-    sprintf(szTempBuffer, "A  %4.1f", dMotorAmps);
-    usClearWidth= strlen(szTempBuffer) * usCharWidth;
-    usCursorY += usLineHeight;
-    DisplayLine(FreeMonoBold12pt7b, WROVER_YELLOW, usCursorX, usCursorY, usClearWidth, usClearHeight, szTempBuffer);
-
-    //sprintf(szTempBuffer, "W %3.0f", dMotorWatts);
-    sprintf(szTempBuffer, "W %4.1f", dMotorWatts);
-    usClearWidth= strlen(szTempBuffer) * usCharWidth;
-    usCursorY += usLineHeight;
-    DisplayLine(FreeMonoBold12pt7b, WROVER_YELLOW, usCursorX, usCursorY, usClearWidth, usClearHeight, szTempBuffer);
-  } //if(bMotorChanged)
-  return;
-}  //DisplayMotor
-
-void DisplayButtons() {
-  const GFXfont   *pFont          = &FreeSans9pt7b;
-  UINT16          usCursorX       = 260;
-  UINT16          usCursorY       = 160;
-  UINT8           ucSize          = 1;
-  UINT16          usColor         = WROVER_ORANGE;
-  bool            bRightJustify   = false;
-
-  //Show 2 lines at right bottom for U d, D d
-  strcpy(szTempBuffer, "U ");
-  itoa(sButtonCount[sUp]  ,sz100CharString  , 10);
-  strcat(szTempBuffer, sz100CharString);
-  DisplayText( usCursorX, usCursorY, szTempBuffer, pFont, ucSize, usColor);
-
-  strcpy(szTempBuffer, "D ");
-  itoa(sButtonCount[sDown]  ,sz100CharString  , 10);
-  strcat(szTempBuffer, sz100CharString);
-  usCursorY= usCursorY + 20;
-  DisplayText( usCursorX, usCursorY, szTempBuffer, pFont, ucSize, usColor);
-  return;
-}  //DisplayButtons
-*/
 //Last line
