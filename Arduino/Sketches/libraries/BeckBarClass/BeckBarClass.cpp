b5ccfcb39c0423f4d98b7c71b46ffb1f5c99917b
lbeck37 <lbeck37@users.noreply.github.com>
2021-06-02 14:50:48 -0600

    Filling out BarClass and related classes.

--- Arduino/Sketches/libraries/BeckBarClass/BeckBarClass.cpp
+++ Arduino/Sketches/libraries/BeckBarClass/BeckBarClass.cpp
@@ -1,17 +1,39 @@
 const char szSystemFileName[]  = "BeckBarClass.cpp";
-const char szSystemFileDate[]  = "6/2/21a";
+const char szSystemFileDate[]  = "6/2/21b";
 
 //#include <BeckEnviroDataClass.h>
 #include <BeckBarClass.h>
 #include <BeckLogLib.h>
 #include <Streaming.h>
 
-BarClass::BarClass(BarType eBar) {
+//BarClass::BarClass(BarType eBar, PUnit XLeft, PUnit YBottom) {
+BarClass::BarClass(BarDataClass BarData) {
   Serial << "BarClass::BarClass(): " << szSystemFileName << ", " << szSystemFileDate << endl;
-  Serial << LOG0 << "BarClass::BarClass(): Set BarType to: " << eNewBarType << endl;
-  eBarType= eBar;
+  Serial << LOG0 << "BarClass::BarClass(): Set BarType to: " << BarData.eBarType << endl;
+  eBarType= BarData.eBarType;
   switch(eBarType){
   case eCO2Bar:
+    BarSegmentClass   GreenSegment= BarSegmentClass();
+    GreenSegment.StartPercent   = 0;
+    GreenSegment.SegmentColor   = BECK_GREEN;
+    GreenSegment.fStartValue    =   0.0;
+    GreenSegment.fEndValue      = 600.0;
+    //BarSegments.push_back(GreenSegment);
+    BarData.BarSegments.push_back(GreenSegment);
+
+    BarSegmentClass   YellowSegment= BarSegmentClass();
+    GreenSegment.StartPercent   = 33;
+    GreenSegment.SegmentColor   = BECK_YELLOW;
+    GreenSegment.fStartValue    =  600.0;
+    GreenSegment.fEndValue      = 1000.0;
+    BarData.BarSegments.push_back(YellowSegment);
+
+    BarSegmentClass   RedSegment= BarSegmentClass();
+    GreenSegment.StartPercent   = 66;
+    GreenSegment.SegmentColor   = BECK_RED;
+    GreenSegment.fStartValue    = 1000.0;
+    GreenSegment.fEndValue      = 2000.0;
+    BarData.BarSegments.push_back(YellowSegment);
     break;
   case eVOCBar:
     break;
--- Arduino/Sketches/libraries/BeckBarClass/BeckBarClass.h
+++ Arduino/Sketches/libraries/BeckBarClass/BeckBarClass.h
@@ -1,6 +1,7 @@
 // BeckBarClass.h, 6/2/21a
 #pragma once
 #include <BeckBiotaDefines.h>
+#include <stdlib.h>
 #include <vector>
 
 enum OrientationType{
@@ -25,7 +26,23 @@ PUnit     StdBarLength  = 120;
 const PUnit     CO2Bar_XLeft     = 200;
 const PUnit     CO2Bar_YBottom   = 30;
 
-/*
+class BarSegmentClass{
+public:
+  BarSegmentClass            ();
+  virtual ~BarSegmentClass   ();
+
+  uint16_t          StartPercent    = 33;
+  ColorType         SegmentColor    = BECK_YELLOW;
+  float             fStartValue     =  600.0;
+  float             fEndValue       = 1000.0;
+  PUnit             XLeft           = 0;
+  PUnit             YBottom         = 0;
+  PUnit             Width           = StdBarWidth;
+  PUnit             Length          = StdBarLength;
+protected:
+};  //BarSegmentClass
+
+
 class BarDataClass{
 public:
   BarDataClass            ();
@@ -37,29 +54,17 @@ public:
   PUnit             YBottom               = 0;
   PUnit             Width                 = StdBarWidth;
   PUnit             Length                = StdBarLength;
-  float             fLowValue             = 0.0;
-  float             fHighValue            = 0.0;
+  float             fStartValue           = 0.0;
+  float             fEndValue             = 0.0;
+  std::vector<BarSegmentClass>  BarSegments;
 protected:
 };  //BarDataClass
-*/
-
-
-class BarSegmentClass{
-public:
-  BarSegmentClass            ();
-  virtual ~BarSegmentClass   ();
-
-  uint16_t          StartPercent    = 33;
-  ColorType         BarColor        = BECK_YELLOW;
-  float             fStartValue     =  600.0;
-  float             fEndValue       = 1000.0;
-protected:
-};  //BarSegmentClass
 
 
 class BarClass{
 public:
-  BarClass            (BarType eBar);
+  //BarClass            (BarType eBar, PUnit XLeft, PUnit YBottom);
+  BarClass            (BarDataClass BarData);
   virtual ~BarClass   ();
 
   //void  Setup         (void);
@@ -67,12 +72,14 @@ public:
   bool  DrawBar       (void);
 
 protected:
+  BarDataClass      _BarData;
   BarType           eBarType              = eNoBar;
 /*
   int16_t           NumSegments           = 3;
   BarSegmentClass   BarSegments           [NumSegments];
 */
-  vector<BarSegmentClass>   BarSegments;
+/*
+  std::vector<BarSegmentClass>            BarSegments;
   OrientationType   Orientation           = eHorizontal;
   PUnit             XLeft                 = 0;
   PUnit             YBottom               = 0;
@@ -81,5 +88,6 @@ protected:
   float             fLowValue             = 0.0;
   float             fHighValue            = 0.0;
   float             fLastDisplayedValue   = 0.0;
+*/
 };  //BarClass
 //Last line.
