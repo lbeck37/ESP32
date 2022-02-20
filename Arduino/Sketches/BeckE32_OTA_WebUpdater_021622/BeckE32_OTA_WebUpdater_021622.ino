565fcd4831e49bbd63e7b0214bda3257efd7ba82
Larry Beck <lbeck37@gmail.com>
2022-02-19 18:04:28 -0800

    Gram: Bringing up array of MAX31855 objects

--- Arduino/Sketches/BeckE32_OTA_WebUpdater_021622/BeckE32_OTA_WebUpdater_021622.ino
+++ Arduino/Sketches/BeckE32_OTA_WebUpdater_021622/BeckE32_OTA_WebUpdater_021622.ino
@@ -1,17 +1,25 @@
 // LastMinuteEngineers.com
 //Works with ESP32 1.0.6, 2/17/22
 const char szSketchName[]  = "BeckE32_OTA_WebUpdater_021622.ino";
-const char szFileDate[]    = "2/17/22s";
+const char szFileDate[]    = "2/19/22a";
 
-#define DO_TEMP       true
-#define DO_WEBSERVER  true
+#define DO_MAX6675      false
+#define DO_MAX31855     true
+#define DO_WEBSERVER    true
 
 #if DO_WEBSERVER
   #include <BeckE32_OTALib.h>
 #endif
-#if DO_TEMP
-  #include <max6675.h>
-#endif
+
+//#if DO_TEMP
+  #if DO_MAX6675
+    #include <max6675.h>
+  #endif
+  #if DO_MAX31855
+    #include <MAX31855.h>
+  #endif
+//#endif
+
 #include <WiFi.h>
 #include <Streaming.h>
 
@@ -25,13 +33,26 @@ const char* szWebHostName = "OTADemo";
 static const byte      cSPI_MISO_Pin         = 19;
 static const byte      cSPI_MOSI_Pin         = 23;
 static const byte      cSPI_CLK_Pin          = 18;
+/*
 static const byte      cSPI_CS_Thermo1Pin    =  5;
 static const byte      cSPI_CS_Thermo2Pin    =  4;
+static const byte      cSPI_CS_Thermo3Pin    =  2;
+*/
+static uint8_t   acSPI_CS_Pins[4]= {0, 5, 4, 2};
 
-#if DO_TEMP
-  //MAX6675   Thermo1(cSCLK, cMOSI_CS, cMISO);
-MAX6675   Thermo1(cSPI_CLK_Pin, cSPI_CS_Thermo1Pin, cSPI_MISO_Pin);
-MAX6675   Thermo2(cSPI_CLK_Pin, cSPI_CS_Thermo2Pin, cSPI_MISO_Pin);
+#if DO_MAX31855
+MAX31855  MAX31855_Object0(acSPI_CS_Pins[0]);
+MAX31855  MAX31855_Object1(acSPI_CS_Pins[1]);
+MAX31855  MAX31855_Object2(acSPI_CS_Pins[2]);
+MAX31855  MAX31855_Object3(acSPI_CS_Pins[3]);
+
+MAX31855  aThermos[4]= {MAX31855_Object0, MAX31855_Object1, MAX31855_Object2, MAX31855_Object3};
+#endif
+
+#if DO_MAX6675
+  MAX6675   Thermo1(cSPI_CLK_Pin, cSPI_CS_Thermo1Pin, cSPI_MISO_Pin);
+  MAX6675   Thermo2(cSPI_CLK_Pin, cSPI_CS_Thermo2Pin, cSPI_MISO_Pin);
+  MAX6675   Thermo3(cSPI_CLK_Pin, cSPI_CS_Thermo3Pin, cSPI_MISO_Pin);
 #endif
 
 void setup(void) {
@@ -48,12 +69,16 @@ void setup(void) {
   }
   Serial << endl << "setup(): Connected to " << szRouterName << ", IP address to connect to is " << WiFi.localIP() << endl;
 
+#if DO_MAX31855     //MAX6675 (obsolete) thermocouple reader doesn't have a setup routine
+  Thermo1.begin();
+  Thermo2.begin();
+  Thermo3.begin();
+#endif
+
 #if DO_WEBSERVER
   Serial << "setup(): Call SetupWebServer(" << szWebHostName << ")" << endl;
   SetupWebserver(szWebHostName);
 #endif
-  //Serial << "setup(): Call SetupWebServer(" << szWebHostName << ")" << endl;
-  //SetupWebserver(szWebHostName);
 
   Serial << "setup(): Done" << endl;
   return;
@@ -61,13 +86,19 @@ void setup(void) {
 
 
 void loop(void) {
-  double    dfDegF1= 37.01;
+  float    dfDegF1= 37.01;
   double    dfDegF2= 37.02;
-#if DO_TEMP
+  double    dfDegF3= 37.02;
+
+#if DO_MAX31855
+  dfDegF1= Thermo1.getTemperature(rawValue= MAX31855_FORCE_READ_DATA);
+#endif
+
+#if DO_MAX6675
   dfDegF1= Thermo1.readFahrenheit();
   dfDegF2= Thermo2.readFahrenheit();
 #endif
-  Serial << "Loop(): Degrees F1= " << dfDegF1 << ", Thermo2= " << dfDegF2 << endl;
+ Serial << "Loop(): Degrees F1= " << dfDegF1 << ", Thermo2= " << dfDegF2 << endl;
   delay(1000);
 
 #if DO_WEBSERVER
