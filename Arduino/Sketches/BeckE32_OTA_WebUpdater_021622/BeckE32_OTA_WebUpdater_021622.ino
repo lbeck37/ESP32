213d11940abdf25dda124fc51b488bf9f42f97da
Larry Beck <lbeck37@gmail.com>
2022-02-19 19:28:28 -0800

    Gram: The Thermocouple is hooked up but reports 2,000 degrees just like the other 2 which are not hooked up.

--- Arduino/Sketches/BeckE32_OTA_WebUpdater_021622/BeckE32_OTA_WebUpdater_021622.ino
+++ Arduino/Sketches/BeckE32_OTA_WebUpdater_021622/BeckE32_OTA_WebUpdater_021622.ino
@@ -1,7 +1,7 @@
 // LastMinuteEngineers.com
 //Works with ESP32 1.0.6, 2/17/22
 const char szSketchName[]  = "BeckE32_OTA_WebUpdater_021622.ino";
-const char szFileDate[]    = "2/19/22b";
+const char szFileDate[]    = "2/19/22g";
 
 #define DO_MAX6675      false
 #define DO_MAX31855     true
@@ -11,14 +11,12 @@ const char szFileDate[]    = "2/19/22b";
   #include <BeckE32_OTALib.h>
 #endif
 
-//#if DO_TEMP
-  #if DO_MAX6675
-    #include <max6675.h>
-  #endif
-  #if DO_MAX31855
-    #include <MAX31855.h>
-  #endif
-//#endif
+#if DO_MAX6675
+  #include <max6675.h>
+#endif
+#if DO_MAX31855
+  #include <MAX31855.h>
+#endif
 
 #include <WiFi.h>
 #include <Streaming.h>
@@ -33,13 +31,8 @@ const char* szWebHostName = "OTADemo";
 static const byte      cSPI_MISO_Pin         = 19;
 static const byte      cSPI_MOSI_Pin         = 23;
 static const byte      cSPI_CLK_Pin          = 18;
-/*
-static const byte      cSPI_CS_Thermo1Pin    =  5;
-static const byte      cSPI_CS_Thermo2Pin    =  4;
-static const byte      cSPI_CS_Thermo3Pin    =  2;
-*/
-static const int      wNumberOfThermos      = 3;
-static uint8_t   acSPI_CS_Pins[wNumberOfThermos + 1]= {0, 5, 4, 2};
+static const int       wNumberOfThermos      =  3;
+static const uint8_t   acSPI_CS_Pins[wNumberOfThermos + 1]= {0, 5, 4, 2};
 
 #if DO_MAX31855
 MAX31855  MAX31855_Object0(acSPI_CS_Pins[0]);     //Dummy so we can call thermocouples 1, 2 and 3
@@ -61,9 +54,8 @@ void setup(void) {
 
   Serial << endl << "setup(): Sketch: " << szSketchName << ", " << szFileDate << endl;
 
-  // Connect to WiFi network
+  // Start WiFi and wait for connection to the network
   WiFi.begin(szRouterName, szRouterPW);
-  // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -80,32 +72,37 @@ void setup(void) {
   Serial << "setup(): Call SetupWebServer(" << szWebHostName << ")" << endl;
   SetupWebserver(szWebHostName);
 #endif
-
   Serial << "setup(): Done" << endl;
   return;
 } //setup
 
 
 void loop(void) {
-  float     afDegreeC[4]= { 37.00, 37.01, 37.02, 37.03};
+  int32_t   wRawData;
+  float     afDegreeC[wNumberOfThermos + 1]= { 37.00, 37.01, 37.02, 37.03};
 
 #if DO_MAX31855
-  int32_t   wRawData= aThermos[1].readRawData();
-  afDegreeC[1]= aThermos[1].getTemperature(wRawData);
+  for (int wThermo= 1; wThermo <= wNumberOfThermos; wThermo++){
+    //Serial << "loop(): Call readRawData()" << endl;
+    wRawData= aThermos[wThermo].readRawData();
+
+    //Serial << "loop(): Call getTemperature()" << endl;
+    afDegreeC[wThermo]= aThermos[wThermo].getTemperature(wRawData);
+
+    Serial << "loop(): Thermocouple Number " << wThermo << " is at " << afDegreeC[wThermo] << " Degrees C" << endl;
+  } //for
 #endif
 
 #if DO_MAX6675
   dfDegF1= Thermo1.readFahrenheit();
   dfDegF2= Thermo2.readFahrenheit();
+  Serial << "Loop(): Degrees F1= " << dfDegF1 << ", Thermo2= " << dfDegF2 << endl;
 #endif
- Serial << "Loop(): Degrees F1= " << dfDegF1 << ", Thermo2= " << dfDegF2 << endl;
   delay(1000);
 
 #if DO_WEBSERVER
   HandleOTAWebserver();
 #endif
-  //HandleOTAWebserver();
-  //delay(1);
   return;
 } //loop
 //Last Line.
