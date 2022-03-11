fd2a72cb737f9140e59c4fb8d8666b6e593d38b5
Larry Beck <lbeck37@gmail.com>
2022-03-11 11:22:52 -0800

    Gram: I stopped a commit before these files made it

---
+++ Arduino/Sketches/BeckE32_Test_I2C/BeckE32_Test_I2C.ino
@@ -0,0 +1,81 @@
+const char szSketchName[]  = "BeckE32_Test_I2C.ino";
+const char szFileDate[]    = "3/11/22d";
+//Beck: This uses a basic test case format which has WiFi and OTA support
+//Program to test MAX6675 Thermocouple reader using SPI
+//Good basis for any simple test
+// this example is public domain. enjoy!
+// www.ladyada.net/learn/sensors/thermocouple
+
+#define DO_OTA          true
+
+#if DO_OTA
+  #include <BeckE32_OTALib.h>
+  #include <WiFi.h>
+#endif
+
+#include<BeckI2cClass.h>
+#include <Streaming.h>
+
+const char* szRouterName  = "Aspot24b";
+const char* szRouterPW    = "Qazqaz11";
+const char* szWebHostName = "Test_MAX6675";
+
+//Pin numbers on WROVER-Kit
+int8_t    cSPI_CLK    = 18;
+int8_t    cSPI_MISO   = 19;
+int8_t    cSPI_CS1    =  2;
+int8_t    cSPI_CS2    =  4;
+int8_t    cSPI_CS3    =  5;
+
+BeckI2cClass      oI2C;
+//Protos
+void setup      ();
+void loop       ();
+void SetupCode  ();
+void LoopCode   ();
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial << endl << "setup(): Begin " << szSketchName << ", " << szFileDate << endl;
+  delay(500);
+#if DO_OTA
+  // Start WiFi and wait for connection to the network
+  WiFi.begin(szRouterName, szRouterPW);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial << endl << "setup(): Connected to " << szRouterName << ", IP address to connect to is " << WiFi.localIP() << endl;
+  Serial << "setup(): Call SetupWebServer(" << szWebHostName << ")" << endl;
+  SetupWebserver(szWebHostName);
+#endif
+  SetupCode();
+  return;
+} //setup
+
+
+void loop() {
+#if DO_OTA
+  HandleOTAWebserver();
+#endif
+  LoopCode();
+  return;
+} //loop
+
+
+void SetupCode() {
+  //Put code for setting up test here.
+  oI2C.Setup();
+  oI2C.ScanForDevices();
+  return;
+} //SetupCode
+
+
+void LoopCode() {
+  //Put code here to do every time loop is called.
+  oI2C.ScanForDevices();
+  delay(1000);
+  return;
+} //LoopCode
+//Last line.
