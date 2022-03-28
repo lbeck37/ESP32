e48c6c02ab488161cf814130269d7b3f0a273a6f
Larry Beck <lbeck37@gmail.com>
2022-03-28 11:12:40 -0700

    Gram: Recreated BeckButtonsClass files after changing them to BeckButtonClass

---
+++ Arduino/Sketches/libraries/BeckButtonsClass/BeckButtonsClass.h
@@ -0,0 +1,39 @@
+// BeckButtonsClass.h, 3/28/22a
+#pragma once
+#include <BeckE32_Defines.h>
+#include <EasyButton.h>
+
+#include <functional>
+
+//static int    _wButtonWasPressed;
+
+class BeckButtonsClass{
+public:
+  BeckButtonsClass();
+  virtual ~BeckButtonsClass();
+
+  //void  SetupButtons           ();
+  void          HandleLoop      ();
+  void          ReadButtons      ();
+  void          SetCallback     (EasyButtonBase::callback_t CallbackRoutine);
+  static void   ButtonsCallback  ();
+  //void          DoCallback      ();
+/*
+  static void  onPressed1   	  ();
+  static void  onPressed2      	();
+  static void  onPressed3      	();
+  static void  onPressed4      	();
+*/
+  //static int    _wButtonWasPressed;
+private:
+  //int           _wButtonWasPressed;
+  EasyButton                    _oTestButton;
+  //EasyButtonBase::callback_t    _CarSetCallback;
+  /*
+  EasyButton TireButton1= EasyButton(_cButton_Pin1);
+  EasyButton TireButton2= EasyButton(_cButton_Pin2);
+  EasyButton TireButton3= EasyButton(_cButton_Pin3);
+  EasyButton TireButton4= EasyButton(_cButton_Pin4);
+*/
+};  //BeckButtonsClass
+//Last line.
